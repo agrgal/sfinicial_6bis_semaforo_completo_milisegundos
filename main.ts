48d3293e4756e494c7f93db930e47bae482bf167
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2023-01-22 10:16:43 +0100

    Conseguido

--- main.ts
+++ main.ts
@@ -20,13 +20,16 @@ let totalTiempos = 0
 let tY = 0
 let tG = 0
 led.enable(false)
-tG = 5 * 1000
-tY = 2 * 1000
-let tR = 5 * 1000
+tG = 7 * 1000
+tY = 3 * 1000
+let tR = 7 * 1000
 totalTiempos = tR + (tY + tG)
 let offset = 0
-cadencia = 200
+cadencia = 400
 basic.forever(function () {
-    ts = control.millis() % totalTiempos + offset
+    ts = (control.millis() + offset) % totalTiempos
+    if (ts <= tG && pins.digitalReadPin(DigitalPin.P5) == 0) {
+        offset += tG - ts
+    }
     encender()
 })
